34cc969f5e7b6de1e58d8d7c89844cb58794c8f2
RomanVasylyk <131609378+RomanVasylyk@users.noreply.github.com>
2023-04-25 20:29:07 +0200

    php

--- Vasylyk_projekt/kontakt.php
+++ Vasylyk_projekt/kontakt.php
@@ -31,39 +31,9 @@ include_once "parts/nav.php";
 
 
 
-<footer class="container">
-  <div class="row1">
-    <div class="col-25">
-      <h4>Kto sme</h4>
-      <p>O reštaurácii talianskej kuchyne "Valentino" možno povedať slovami Oscara Wilda: "Chuť je tichá."</p>
-      <p>Toto tvrdenie platí pre elitnú reštauráciu, ktorá dokonale zapadá do atmosféry úzkej uličky a sprostredkúva čaro a pôvab Talianska.</p>
-      <p>Jasné osvetlenie fasády a mramorového schodiska, nádherný kovaný plot, svieže kvety v črepníkoch pozývajú alebo dokonca lákajú vojsť dovnútra bez slov. Vládne tu pôvabná aristokracia a diskrétny luxus.</p>
-    </div>
-    <div class="col-25">
-      <h4>Kontaktujte nás</h4>
-      <ul>
-        <li><i class="fa fa-envelope" aria-hidden="true"><a href="mailto:vasulukroma@gmail.com"> vasulukroma@gmail.com</a></i></li>
-        <li><i class="fa fa-phone" aria-hidden="true"><a href="tel:0909500000"> 0909500000</a></i></li>
-      </ul>
-    </div>
-    <div class="col-25">
-      <h4>Rýchle odkazy</h4>
-      <ul>
-        <li><a href="index.html">Domov</a></li>
-        <li><a href="o_nas.html">Blog</a></li>
-        <li><a href="galeria.html">Galeria</a></li>
-        <li><a href="kontakt.html">Kontakt</a></li>
-      </ul>
-    </div>
-    <div class="col-25">
-      <h4>Nájdete nás</h4>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1350063.94591879!2d16.109879762499993!3d48.631974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4714c91b15521805%3A0xcd5863381d254045!2sRestaurace%20Hoffer!5e0!3m2!1sru!2ssk!4v1669385778130!5m2!1sru!2ssk" style="border:0;width: 100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-     </div>
-  </div>
-  <div class="row1">
-    Created and designed by Roman Vasylyk
-  </div>
-</footer>
+<?php
+include_once "parts/footer.php";
+?>
 
   <script src="js/app .js"></script>
 </body>
