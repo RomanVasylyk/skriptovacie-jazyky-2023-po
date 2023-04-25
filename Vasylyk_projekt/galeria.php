f66f3df962cede39c70ff3f3ec53e01b16939742
RomanVasylyk <131609378+RomanVasylyk@users.noreply.github.com>
2023-04-25 09:56:41 +0200

    php

--- Vasylyk_projekt/galeria.php
+++ Vasylyk_projekt/galeria.php
@@ -13,10 +13,10 @@
     <a href="#"><img class="img" src="./img/images.jpeg" alt="Logo"></a>
     <nav class="main-nav">
       <ul class="main-menu" id="main-menu">
-        <li><a class="d" href="index.html">Domov</a></li>
-        <li><a class="o" href="o_nas.html">Blog</a></li>
-        <li><a class="g" href="galeria.html">Galeria</a></li>
-        <li><a class="k" href="kontakt.html">Kontakt</a></li>
+        <li><a class="d" href="index.php">Domov</a></li>
+        <li><a class="o" href="o_nas.php">Blog</a></li>
+        <li><a class="g" href="galeria.php">Galeria</a></li>
+        <li><a class="k" href="kontakt.php">Kontakt</a></li>
       </ul>
       <a class="hamburger" id="hamburger">
         <i class="fa fa-bars"></i>
